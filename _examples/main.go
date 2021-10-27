1cea8d62db3a59b39c92bb1e63a46988ca21a20a
kubaroth <kubaroth@gmail.com>
2021-10-26 23:03:33 -0700

    comparing ray_hit() with Sphere.Hit() - WIP

--- _examples/main.go
+++ _examples/main.go
@@ -25,7 +25,7 @@ import (
 type HitRecord struct {
 	P, Normal Vec3 // point and normal
 	T float32
-	
+	FrontFace bool
 }
 
 // Polymorphism example
@@ -42,8 +42,6 @@ type Hittable interface {
 }
 
 func (s Sphere) Hit(r *Ray, t_min, t_max float32, rec *HitRecord) bool {
-	fmt.Println("A")
-
 	oc := r.Origin().Subtr(s.Center)
 	a := r.Direction().Dot(r.Direction())
 	half_b := oc.Dot(r.Direction())
@@ -64,12 +62,30 @@ func (s Sphere) Hit(r *Ray, t_min, t_max float32, rec *HitRecord) bool {
             return false
 		}
     }
-	
+	// fmt.Println("rootNew", root)
     rec.T = root;
     rec.P = r.At(rec.T);
-    rec.Normal = (rec.P.Subtr(s.Center)).DivF(s.Radius)
+	// fmt.Println("P New", rec.P.Subtr(s.Center), r.At(root).Subtr(s.Center), root, s.Center)
+    // rec.Normal = (rec.P.Subtr(s.Center)).DivF(s.Radius)
+    outward_normal := (rec.P.Subtr(s.Center)).DivF(s.Radius)
+	// rec.Normal = (rec.P.Subtr(s.Center))
+	// outward_normal := (rec.P.Subtr(s.Center))
+    rec.set_face_normal(r, &outward_normal)
+	
 	return true;
 }
+
+func (rec *HitRecord) set_face_normal(r *Ray, outward_normal * Vec3) {
+	if r.Direction().Dot(*outward_normal) < 0{ // TODO: this may reqiure flip
+		rec.Normal = *outward_normal
+		rec.FrontFace = true
+	} else {
+		rec.Normal = NewVec3(-outward_normal.At(0), -outward_normal.At(1), -outward_normal.At(2))
+		rec.FrontFace = false
+	}
+	//normal = front_face ? outward_normal :-outward_normal;
+}
+
 
 func (b B) Hit(r *Ray, t_min, t_max float32, rec *HitRecord) bool{
 	fmt.Println("B")
@@ -92,12 +108,52 @@ func hit_sphere(center *Vec3, radius float32, r *Ray) float64 {
 	if discriminant < 0{
 		return -1.0
 	} else {
-		return float64(-half_b - float32(math.Sqrt(discriminant))) / (float64(a))
+		root := float64(-half_b - float32(math.Sqrt(discriminant))) / (float64(a)) 
+		fmt.Println("root_old", root)
+		return root
 	}
 
 }
 
-func ray_color(r *Ray) color.RGBA {
+func ray_color(r *Ray, sphere *Sphere) color.RGBA {
+
+
+	aa := NewVec3(0, 0, -1)
+	root := float32(hit_sphere(&aa, 0.5, r))
+	if root > 0 {
+		aa = r.At(float32(root)).Subtr( NewVec3(0,0,-1) )
+		// fmt.Println("P Old", r.At(float32(root)), aa, root, NewVec3(0,0,-1))
+		// aa = aa.UnitVec()
+	}
+	
+	rec := HitRecord{NewVec3(0,0,0), NewVec3(0,0,0), 1.0, true}
+	hit := sphere.Hit(r, 0.0, 100000, &rec)
+	
+	if hit { 
+		N := (rec.Normal.Add(NewVec3(1,1,1))).MultF(float32(0.5))
+		// N = N.UnitVec()
+		R := N.At(0)
+		G := N.At(1)
+		B := N.At(2)
+
+		// N := rec.Normal.UnitVec()
+		// R := (N.At(0) + 1) * 0.5
+		// G := (N.At(1) + 1) * 0.5
+		// B := (N.At(2) + 1) * 0.5
+		
+	return color.RGBA{uint8(R*255), uint8(G*255), uint8(B*255), 255}
+		
+	}
+	/*
+
+	   hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+	
 	aa := NewVec3(0, 0, -1)
 	t := float32(hit_sphere(&aa, 0.5, r))
 	if t > 0 {
@@ -108,14 +164,17 @@ func ray_color(r *Ray) color.RGBA {
 		B := (N.At(2) + 1) * 0.5
 		return color.RGBA{uint8(R*255), uint8(G*255), uint8(B*255), 255}
 	}
+	*/
+
+	
 
 	unit_direction := r.Direction().UnitVec()
-	t = float32(0.5 * (unit_direction.At(1) + 1.0))
+	t := float32(0.5 * (unit_direction.At(1) + 1.0))
 	sky := NewVec3(0.5, 0.7, 1.0).MultF(t)
 	sky = sky.Add(NewVec3(1, 1, 1).MultF(1 - t))
 	sky = sky.MultF(255) // NOTE: rember to shift range to 0-255
 	return color.RGBA{uint8(sky.At(0)), uint8(sky.At(1)), uint8(sky.At(2)), 255}
-	
+
 
 }
 
@@ -132,13 +191,15 @@ func main() {
         defer pprof.StopCPUProfile()
     }
 
-	rec := HitRecord{NewVec3(0,0,0), NewVec3(0,0,0), 1.0}
+	rec := HitRecord{NewVec3(0,0,0), NewVec3(0,0,0), 1.0, true}
 	ray := NewRay(NewVec3(0,0,0), NewVec3(0,0,0))
 	a := []Hittable{Sphere{NewVec3(0,0,0), 1.0}, B{2}, Sphere{NewVec3(1,1,1), 0.5}}
 	for _, i := range a {
 		i.Hit(&ray, 0, 1, &rec)
 	}
 
+	sphere := Sphere{NewVec3(0,0,-1), 0.5}
+	
 	path := os.Getenv("HOME") + "/storage/downloads/img.png"
 	fmt.Println("saving into:", path)
 
@@ -149,7 +210,7 @@ func main() {
 
 	//Image
 	aspect_ratio := 16.0 / 9.0
-	width := 2000
+	width := 200
 	height := int(float64(width) / aspect_ratio)
 	fmt.Printf("image res", width, height)
 	
@@ -190,7 +251,7 @@ func main() {
 			
 			ray := NewRay(origin, dir)
 			// fmt.Println("ray", ray)
-			cd := ray_color(&ray)
+			cd := ray_color(&ray, &sphere)
 			// fmt.Println(i, j)
 			img.SetRGBA(i, height-j, cd)
 		}
